7a8c27e7e92b48e51557e98db2cd7fe2f15d84ca
Mrtlu3 <mrt.luca3@gmail.com>
2025-10-03 18:49:06 +0200

    riordino dei file

---
+++ proving_c/prova.cpp
@@ -0,0 +1,9 @@
+#include<cstdio>
+#include<stdlib.h>
+double dumb;
+int main(void){
+    printf("ciao, control +1 finisci in editor\n control + ò finisci in terminale \ninserisci un numero\n");
+    scanf("%lf", &dumb);
+    printf("%lf\n", dumb);
+    return 1;
+}
